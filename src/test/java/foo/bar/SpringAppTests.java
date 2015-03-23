f6d880f821085b37eca90c89256bcd0216bbd19c
th3Dot <chaosikcz@gmail.com>
2015-03-23 22:59:00 +0100

    added tests

---
+++ src/test/java/foo/bar/SpringAppTests.java
@@ -0,0 +1,28 @@
+package foo.bar;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:spring-config.xml")
+public class SpringAppTests {
+    @Autowired
+    private HelloService helloService;
+
+    @Autowired
+    private HelloConsumer helloConsumer;
+
+    @Test
+    public void testSayHello() {
+        assertEquals("Hello!", helloService.sayHello());
+    }
+
+    @Test
+    public void testHelloConsumer() {
+        assertEquals("Hello!", helloConsumer.consume());
+    }
+}
